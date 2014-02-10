4737428198b4e180f059896f95605407e9b45744
Wei Li <wei.li@feedhenry.com>
2014-02-10 15:05:41 +0000

    update flurry plugin for android

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Here's how to install the plugin:
 
 1. Register with Flurry and get an App key
 2. Install plugin with plugman or cordova-cli
-3. Call the startSession() method, with your app key, after the device is ready. Or you can add your flurry key to www/config.xml like this (iOS only for now):
+3. Call the startSession() method, with your app key, after the device is ready. Or you can add your flurry key to www/config.xml like this:
 
 ```
 <!-- Flurry App Key -->
@@ -27,3 +27,30 @@ Here's how to install the plugin:
 ```
 
 4. Call the other Flurry methods as appropriate.
+
+
+Start Flurry Session On App Start
+=================================
+
+There are two ways to start a flurry session. One is by calling 
+
+```
+flurry.startSession(flurryKey)
+```
+
+to start the flurry session programatically. The other one is to start the flurry session automatically on app startup.
+
+To do that, all you have to do is to add the above preference to the config.xml file and turn on onload=true in the feature param. For example:
+
+```
+<feature name="Flurry">
+    <param name="ios-package" value="CDVFlurry" />
+    <param onload="true" />
+</feature>
+```
+
+It will check if the flurry key preference exists. If it does, it will start the flurry session.
+
+
+
+
--- src/android/Flurry.java
+++ src/android/Flurry.java
@@ -5,16 +5,91 @@ import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 
+import android.app.Application;
+import android.content.res.XmlResourceParser;
 import android.util.Log;
 
 import com.flurry.android.FlurryAgent;
+import com.urbanairship.Logger;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Flurry extends CordovaPlugin {
+  
+    private boolean flurryEnabled = false;
+    private static final String FLURRY_KEY_STRING = "com.flurry.app_key";
+  
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+      super.initialize(cordova, webView);
+      setup();
+    }
+    
+    private void setup(){
+      String flurryKey = getFlurryKey(this.cordova.getActivity().getApplication());
+      if (null != flurryKey) {
+        FlurryAgent.onStartSession(this.cordova.getActivity(), flurryKey);
+        flurryEnabled = true;
+      }
+    }
+    
+    @Override
+    public void onDestroy() {
+      super.onDestroy();
+      if(flurryEnabled){
+        FlurryAgent.onEndSession(this.cordova.getActivity());
+        flurryEnabled = false;
+      }
+      
+    }
+
+    @Override
+    public void onReset() {
+      super.onReset();
+      if(flurryEnabled){
+        FlurryAgent.onEndSession(this.cordova.getActivity());
+        flurryEnabled = false;
+      }
+    }
+    
+    private String getFlurryKey(Application pApplication){
+      int id = pApplication.getResources().getIdentifier("config", "xml", pApplication.getPackageName());
+      if (id == 0) {
+          return null;
+      }
+      String flurryKey = null;
+      XmlResourceParser xml = pApplication.getResources().getXml(id);
+      int eventType = -1;
+      while (eventType != XmlResourceParser.END_DOCUMENT) {
+
+          if (eventType == XmlResourceParser.START_TAG) {
+              if (xml.getName().equals("preference")) {
+                  String name = xml.getAttributeValue(null, "name").toLowerCase();
+                  String value = xml.getAttributeValue(null, "value");
+
+                  if (name.startsWith(FLURRY_KEY_STRING) && value != null && !value.isEmpty()) {
+                      flurryKey = value;
+                      Logger.verbose("Found " + name + " in config.xml with value: " + value);
+                      break;
+                  }
+              }
+          }
+
+          try {
+              eventType = xml.next();
+          } catch (Exception e) {
+              Logger.error("Error parsing config file", e);
+          }
+      }
+      
+      return flurryKey;
+    }
 
     private Map<String, String> JsonToMap(JSONObject jdata) throws JSONException {
         if(jdata == null){
